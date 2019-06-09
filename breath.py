2f0873d69f51621b6463681810e96a3c8d140152
Oleg Legun <oleg.legun@gmail.com>
2019-06-09 12:28:04 +0300

    update breath

--- breath.py
+++ breath.py
@@ -109,3 +109,66 @@ class Circle:
             self.canvas.fill_ellipse(*self._convert_coords(
                 self.center_x, self.center_y, self.radius * self.__scale_factor))
 
+
+def draw(canvas, length):
+    circles = []
+
+    for i in range(1, CIRCLE_NUMBER + 1):
+        if RICH_COLORS_MODE:
+            color = get_rgb_color_for_number(i, CIRCLE_NUMBER, COLOR_OPACITY)
+        else:
+            color = COLORS['cyan-alpha']
+
+        circles.append(
+            Circle(canvas, CENTER['x'], CENTER['y'], INITIAL_CIRCLE_SIZE,
+                   color))
+
+    for circle in circles:
+        circle.draw()
+
+    for tick in range(length):
+        time_start = time.process_time()
+        log('#' + str(tick).zfill(4), end=' ')
+        canvas.begin_updates()
+        set_background(COLORS['black'])
+
+        circle_path_radius = ease(
+            CIRCLE_PATH_MAX_RADIUS, tick, speed=CIRCLE_PATH_RADIUS_CHANGE_SPEED)
+        circle_rotation_degree = ease(
+            360, tick, speed=ROTATION_SPEED, negative=True)
+
+        for i, circle in enumerate(circles, 1):
+            angle = 360 / CIRCLE_NUMBER * i + circle_rotation_degree
+            x, y = calc_coordinates_on_circle(
+                degree=angle, radius=circle_path_radius)
+            circle.set_coords(CENTER['x'] + x, CENTER['y'] + y)
+
+            if DYNAMIC_COLOR:
+                circle.rotate_color_angle(3)
+            circle.scale(absolute=ease(
+                CIRCLE_MAX_SCALE_FACTOR, tick, speed=CIRCLE_SCALE_SPEED))
+            circle.draw()
+
+        time_end = time.process_time()
+        time_elapsed = time_end - time_start
+        time_remaining = get_remaining_frame_time(time_elapsed, FPS)
+
+        time.sleep(time_remaining)
+
+        if FPS_COUNTER:
+            current_fps = get_fps(time_start, time_end + time_remaining)
+            draw_text(canvas, f'{current_fps} FPS', 'bottom')
+
+        canvas.end_updates()
+
+
+def main():
+    canvas.set_size(WIDTH, HEIGHT)
+    set_background(COLORS['black'])
+    #canvas.set_aa_enabled(False)
+    draw(canvas, SIMULATION_LENGTH)
+
+
+if __name__ == '__main__':
+    main()
+
